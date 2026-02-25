c4474706bf9971dd4a539e0d98a6f4adf0c130b6
Nathan Soares <nathansoares@email.com>
2026-02-25 13:22:24 -0300

    Sale service

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,19 @@
 package com.devsuperior.dsmeta.services;
 
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleDTO;
+import com.devsuperior.dsmeta.dto.SummaryDTO;
+import com.devsuperior.dsmeta.projection.ReportSaleProjection;
+import com.devsuperior.dsmeta.projection.ReportSummaryProjection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -14,10 +25,33 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleDTO> saleReport(String dtInitial, String dtFinal, String name, Pageable pageable){
+
+		LocalDate dataFinal = dtInitial != null?LocalDate.parse(dtFinal):  LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate dataInitial = dtInitial != null?LocalDate.parse(dtInitial): dataFinal.minusYears(1L);
+		name = name != null?name: " ";
+
+		Page<ReportSaleProjection> result = repository.reportSales(Date.valueOf(dataInitial), Date.valueOf(dataFinal), name, pageable);
+
+		Page<SaleDTO> saleDTOPage = result.map(x -> new SaleDTO(x));
+
+		return  saleDTOPage;
+	}
+
+	public Page<SummaryDTO> summaryReport(String dtInitial, String dtFinal, Pageable pageable){
+
+		LocalDate dataFinal = dtInitial != null?LocalDate.parse(dtFinal):  LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate dataInitial = dtInitial != null?LocalDate.parse(dtInitial): dataFinal.minusYears(1L);
+
+		Page<ReportSummaryProjection> result = repository.reportSummary(Date.valueOf(dataInitial), Date.valueOf(dataFinal), pageable);
+
+        return result.map(x -> new SummaryDTO(x));
 	}
 }
